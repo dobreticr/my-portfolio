bc056990f1dd875febef1b65244e8bf339ec7103
Ronaldo <dobreticr233@hotmail.com>
2025-02-06 22:45:37 +0100

    Making password insvisible

--- send_email.py
+++ send_email.py
@@ -1,16 +1,16 @@
 import smtplib, ssl
-
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "dobreticr@gmail.com"
-    password = "hhfeezkdiqqveyaw"
+    password = os.getenv("PASSWORD")
+
     receiver = "dobreticr@gmail.com"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
-
 
